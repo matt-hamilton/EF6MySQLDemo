4bea367ae7c02e615d919ee74af0373c20872b46
Matt Hamilton <11399052+matt-hamilton@users.noreply.github.com>
2021-03-06 07:40:24 +1300

    Init project

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EF6MySQLDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new ModelContext())
+            {
+                var count = context.Cars.Count();
+                Console.WriteLine($"{count} car(s) found");
+                if (count == 0)
+                {
+                    Console.WriteLine("Do you want to add some cars (y/n)");
+
+                    var key = Console.ReadLine().ToLower();
+
+                    if (key == "y")
+                    {
+                        List<Car> cars = new List<Car>();
+
+                        cars.Add(new Car { Manufacturer = "Nissan", Model = "Pathfinder", Year = 2008 });
+                        cars.Add(new Car { Manufacturer = "Honda", Model = "Civic", Year = 2013 });
+                        cars.Add(new Car { Manufacturer = "Volkswagen", Model = "Golf", Year = 2012 });
+
+                        context.Cars.AddRange(cars);
+                        context.SaveChanges();
+
+                        Console.WriteLine("3 cars were added");
+
+                        printCars(context);
+                    }
+                }
+                else
+                {
+                    printCars(context);
+                }
+
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void printCars(ModelContext context)
+        {
+            foreach (var car in context.Cars)
+            {
+                Console.WriteLine($"{car.Year} {car.Manufacturer} {car.Model}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # EF6MySQLDemo
+
+Basic .NET Framework console app working demo using the Entity Data Model using a Database First Approach
+
+### Requirements 
+
+- Connector/NET 8.0.18        
+- MySQL Server 5.7      
+- Entity Framework 6.4.4 
+- MySql.Data 8.0.18.0 
+- MySql.Data.EntityFramework 8.0.18.0 
+- .NET Framework 4.7.2
+- Visual Studio 2019
+- MySql for Visual Studio 1.2.9
+
